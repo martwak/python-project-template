61bf4952f086390b65e26f9b58a02a78563fec21
Maximilian Wassink <wassink.maximilian@protonmail.com>
2021-05-15 13:28:52 +0200

    added meta to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,38 @@
 from setuptools import setup
 
+
+def readme():
+    with open("README.md") as file:
+        return file.read()
+
+
 setup(
     name="Python Project Template",
     version="0.1",
     description="Python Project Template with GitHub actions",
+    long_description=readme(),
+    long_description_content_type="markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Archiving :: Packaging",
+    ],
+    keywords=[
+        "template",
+        "project structure",
+        "github actions",
+        "continious integration",
+    ],
     url="https://github.com/maximilianwassink/python-project-template",
     author="Maximilian Wassink",
     author_email="wassink.maximilian@protonmail.com",
     license="MIT",
     packages=["project_dir"],
+    install_requires=[],
     zip_safe=False,
+    entry_points={},
 )
