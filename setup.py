b789f4a05f6771d8c8f8cf3eb05cf855c43f5731
Maximilian Wassink <wassink.maximilian@protonmail.com>
2021-05-15 13:10:18 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="Python Project Template",
+    verion="0.1",
+    description="Python Project Template with GitHub actions",
+    url="https://github.com/maximilianwassink/python-project-template",
+    author="Maximilian Wassink",
+    author_email="wassink.maximilian@protonmail.com",
+    license="MIT",
+    packages=["project_dir"],
+    zip_safe=False,
+)
